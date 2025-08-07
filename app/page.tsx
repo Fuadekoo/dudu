16105aaecd82e21217a4f2d70e8646aa4f1a425d
Fuadekoo <fuadabdurahman0859@gmail.com>
2025-08-07 22:55:20 +0300

    feat: implement Navbar, Home, About, Contact components and enhance Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,42 @@
 "use client";
 import { Button } from "@heroui/react";
 import React from "react";
+import Navbar from "@/components/navbar";
+import Home from "@/components/home";
+import Contact from "@/components/contact";
+import About from "@/components/about";
+import Footer from "@/components/footer";
 
 function page() {
   return (
-    <div>
-      <h1>Welcome to the Page</h1>
-      <p>This is the content of the page.</p>
-      <Button color="primary">hy fuad</Button>
+    <div className="grid grid-cols-1 gap-4 min-h-svh">
+      {/* this is a navbar */}
+      <div className="sticky top-0 z-10 p-0">
+        <Navbar />
+      </div>
+      <main className="grid grid-cols-1 gap-4">
+        <div className="border-2 rounded h-[calc(100vh-4rem)]">
+          <Home />
+        </div>
+        <div className="border-2 rounded h-[calc(100vh-4rem)]">
+          <About />
+        </div>
+        {/* <div className="border-2 rounded h-[calc(100vh-4rem)]">
+          <Skills />
+        </div>
+        <div className="border-2 rounded h-[calc(100vh-4rem)]">
+          <Work />
+        </div>
+        <div className="border-2 rounded h-[calc(100vh-4rem)]">
+          <Testimonial />
+        </div> */}
+        <div className="border-2 rounded h-[calc(100vh-4rem)]">
+          <Contact />
+        </div>
+      </main>
+      <div className="border-2 rounded">
+        <Footer />
+      </div>
     </div>
   );
 }
